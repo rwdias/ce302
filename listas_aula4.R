6d8c662991b322f26cfc1b65325309cacc39f149
Rafael Dias <rwdias@gmail.com>
2023-08-24 19:28:03 -0300

    aula 5

---
+++ listas_aula4.R
@@ -0,0 +1,14 @@
+# Criando uma lista com diferentes tipos de elementos
+minha_lista <- list(
+  nome = "Alice",
+  idade = 15,
+  notas = c(90, 85, 95),
+  ativo = TRUE, 
+  País = "das Maravilhas", 
+  amigos = "Cheshire", 
+  frase = "How do I know I'm mad?"
+)
+minha_lista
+
+minha_lista$nome
+minha_lista$ativo
